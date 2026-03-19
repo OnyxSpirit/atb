92f9cd7c8d4b75007ba7f0e639e2e99daa8b1d91
OnyxSpirit <robertoachillesdasilva@gmail.com>
2026-03-19 15:09:11 +0100

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,13 @@
 import mysql from 'mysql';
-import doteven from 'dotenv';
-
-doteven.config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 const db = mysql.createConnection({
-    host:'http://sql206.ezyro.com/',
-    user:'ezyro_41219582',
-    password:'JesusChrist_5',
-    database:'ezyro_41219582_atb'
+    host: process.env.VITE_HOST,
+    user:process.env.VITE_USER,
+    password:process.env.VITE_PASSWORD,
+    database:process.env.VITE_DATABASE'
+    port:process.env.VITE_PORT
 });
-/*
-host: process.env.VITE_HOST || 'cv1vlj.myd.infomaniak.com',
-    user:process.env.VITE_USER || 'cv1vlj_atb_user',
-    password:process.env.VITE_PASSWORD || 'JesusChrist_5',
-    database:process.env.VITE_DATABASE || 'cv1vlj_atb'
-*/
+
 export default db;
