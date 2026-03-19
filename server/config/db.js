1b626c3dfe33ee12e4a1203f770c0109bb0bfbf1
OnyxSpirit <robertoachillesdasilva@gmail.com>
2026-03-19 12:04:58 +0100

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,7 @@ import doteven from 'dotenv';
 doteven.config();
 
 const db = mysql.createConnection({
-    host: process.env.VITE_HOST || 'http://cv1vlj.myd.infomaniak.com',
+    host: process.env.VITE_HOST || 'cv1vlj.myd.infomaniak.com',
     user:process.env.VITE_USER || 'cv1vlj_atb_user',
     password:process.env.VITE_PASSWORD || 'JesusChrist_5',
     database:process.env.VITE_DATABASE || 'cv1vlj_atb'
