7b88f70e7841dab49c59e4caa6b00462b5e1e460
OnyxSpirit <robertoachillesdasilva@gmail.com>
2026-03-19 14:12:26 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ import session from "express-session";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
-import {db} from "./config/db.js";
+import db from "./config/db.js";
 import multer from "multer";
 import path from "path";
 import fs from "fs";
